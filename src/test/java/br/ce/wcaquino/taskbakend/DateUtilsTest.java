4489c6aa33273c44350478d6d23b38e9f1748362
menichini <paulomenichini@gmail.com>
2024-05-23 20:04:06 -0400

    Melhoria testes unitários

--- src/test/java/br/ce/wcaquino/taskbakend/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbakend/DateUtilsTest.java
@@ -25,4 +25,11 @@ public class DateUtilsTest {
         assertTrue(result);
     }
 
+    @Test
+    public void isEquals() {
+        LocalDate before = LocalDate.now();
+        boolean result = DateUtils.isEqualOrFutureDate(before);
+        assertTrue(result);
+    }
+
 }
